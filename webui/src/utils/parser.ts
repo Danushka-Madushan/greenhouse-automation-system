88c7a343c719c838aee140d73d9d3f19114519f3
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-06-28 18:56:22 +0530

    feat: create parser.ts

---
+++ webui/src/utils/parser.ts
@@ -0,0 +1,12 @@
+export class Parser {
+  public static parseTempHumidity: (statusStr: string) => { temperature: number; humidity: number } = (statusStr) => {
+    // 1. Get everything after the last colon, then split by the comma
+    const [temp, humidity] = statusStr.split(':').pop()?.split(',') ?? ['0', '0'];
+
+    // 2. Convert strings to numbers and return an object
+    return {
+      temperature: parseFloat(temp),
+      humidity: parseFloat(humidity)
+    };
+  };
+}
