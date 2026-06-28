531506ab8847048775a1b82b750caa983fe7f99f
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-06-28 18:56:33 +0530

    update GreenOSEvents.cs

--- uart-com/Constants/GreenOSEvents.cs
+++ uart-com/Constants/GreenOSEvents.cs
@@ -6,23 +6,43 @@ namespace uart_com.Constants
     {
       public static class Incoming
       {
-        public const string TEMP_HUMIDITY_DATA_DYN = "STATUS:TEMP_HUMIDITY:";
-        public const string ERROR_DHT22_MSG_DYN = "ERR:SENSOR_DH22:";
+        public static class Ardiono
+        {
+          /* Sensor Updates */
+          public const string TEMP_HUMIDITY_DATA_DYN = "STATUS:TEMP_HUMIDITY:";
+          /* Sensor Errors */
+          public const string ERROR_DHT22_MSG_DYN = "ERR:SENSOR_DH22:";
 
-        public const string GREENHOUSE_UNO = "SYS:GREENHOUSE_UNO";
+          /* System Check */
+          public const string GREENHOUSE_UNO = "SYS:GREENHOUSE_UNO";
+        }
 
-        /* FromWebUI */
-        public const string SYS_IS_ONLINE = "SYS:IS_ONLINE";
+        public static class WebUI
+        {
+
+        }
       }
 
       public static class Emit
       {
-        public const string WHOAMI = "SYS:WHOAMI\n";
+        public static class Ardiono
+        {
+          /* System Check */
+          public const string WHOAMI = "SYS:WHOAMI\n";
+        }
 
-        /* ToWebUI */
-        public const string SYS_ONLINE = "SYS:ONLINE";
-        public const string SYS_OFFLINE = "SYS:OFFLINE";
-        
+        public static class WebUI
+        {
+          /* ToWebUI */
+          public const string SYS_ONLINE = "SYS:ONLINE";
+          public const string SYS_OFFLINE = "SYS:OFFLINE";
+
+          /* Sensor Updates */
+          public const string UPDATE_TEMP_HUMIDITY = "onSensorUpdate:TEMP_HUMIDITY";
+          /* Sensor Errors */
+          public const string TEMP_HUMIDITY_ERROR = "onSensorError:TEMP_HUMIDITY";
+        }
+
       }
     }
   }
