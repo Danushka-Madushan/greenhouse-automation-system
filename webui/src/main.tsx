7fedd4112fa38bd0442eff061ae9a5dd67be59b1
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-06-29 22:35:38 +0530

    update main.tsx

--- webui/src/main.tsx
+++ webui/src/main.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Toast } from '@heroui/react/toast'
+import { Fragment } from 'react/jsx-runtime'
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <Fragment>
+    <Toast.Provider />
+    <App />
+  </Fragment>
 )
